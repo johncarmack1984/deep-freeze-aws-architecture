3634b4fbfeda8125277c00926cc6d6701cb2390a
John Carmack <johncarmack@me.com>
2023-06-15 19:41:41 -0700

    Update AWS and Random providers to latest versions

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "5.3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "3.5.1"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.0"
 
   cloud {
     organization = "john-carmack"
